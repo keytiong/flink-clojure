8148baa8bd343e52eb913277deccee9dff9e2fef
Key Tiong Tan <keytiong@hotmail.com>
2022-05-12 21:31:33 +0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.kosong.flink/flink-clojure "0.1.0-SNAPSHOT"
+(defproject io.kosong.flink/flink-clojure "0.1.0"
   :description "Clojure wrapper for Apache Flink"
   :url "https://github.com/keytiong/flink-clojure"
   :license {:name "The MIT License"
